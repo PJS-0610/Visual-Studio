9e801a8843e6b788d9af9b5b672f2d2de9de77ca
JunSeong Park <ranige0610@naver.com>
2023-01-16 19:54:40 +0900

    3단원 진행중

---
+++ Ch03/Prj06/float.c
@@ -0,0 +1,18 @@
+/*
+	솔루션 / 프로젝트 / 소스파일: Ch03 / Prj06 / float.c
+	부동소수현 변수의 선언과 활용
+	V 1.0 2022.
+	*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+	float x = 3.14F;
+	double y = -3.141592;
+	long double z = 180000000.0;
+	printf("저장값: %f %f %f\n", x, y, z);
+
+	return 0;
+}
